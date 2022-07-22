6c39cd1a25d1dbff8a211a9f434b991763d26564
Vladimir Drozdov <mayakovsky03@gmail.com>
2022-07-22 14:45:04 +0300

    lessonsJs 11-20

---
+++ script.js
@@ -0,0 +1,45 @@
+// 11. Add two numbers with JS 
+
+const sum = 10 + 10;
+
+// 12. Subtract one number from another with JS
+
+const difference = 45 - 33;
+
+// 13. Multiply two numbers with JS
+
+const product = 8 * 10;
+
+// 14. Divide one number by another with JS
+
+const quotient = 66 / 33;
+
+// 15. Increment a Number with JS
+
+// let myVar = 87;
+
+myVar++;
+
+// 16. Decrement a Number with JS
+
+// let myVar = 11;
+
+myVar--;
+
+// 17. Create Decimal Numbers with JS
+
+const ourDecimal = 5.7;
+
+const myDecimal = 2.2;
+
+// 18. Multiply Two Decimals with JavaScript
+
+// const product = 2.0 * 2.5;
+
+// 19. Divide One Decimal by Another with JS
+
+// const quotient = 4.4 / 2.0;
+
+// 20. Finding a Remainder in JavaScript
+
+const remainder = 11 % 3;
